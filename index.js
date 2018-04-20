ce45a383b3c30a8bc5beb95c3268ad00c07fd2a7
Brendan <bstrallow@gmail.com>
2018-04-19 20:38:26 -0400

    Finished part 2.

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ $(document).ready(function () {
             finalHTML += '<div class="movie card">';
             finalHTML += '<img src="' + currentMovie.Poster + '" class="card-img-top" />';
             finalHTML += '<div class="card-body">';
-            finalHTML += '<h5 class="card-title movieTitle">' + currentMovie.title + '</h5>';
+            finalHTML += '<h5 class="card-title movieTitle">' + currentMovie.Title + '</h5>';
             finalHTML += '<h6 class="card-subtitle text-muted movieYear">' + currentMovie.Year + '</h6>';
-            finalHTML += '<a href="#"><button type="button" class="btn-primary">Add!</button></a>';
+            finalHTML += '<button type="button" class="btn-primary addButton" data-id="' + currentMovie.imdbID + '">Add!</button>';
             finalHTML += '</div>';
             finalHTML += '</div>';
             
@@ -19,5 +19,20 @@ $(document).ready(function () {
         e.preventDefault();
         var movieHTML = renderMovies(movieData);
         $('.movies-container').html(movieHTML);
+    });
+    $('.movies-container').on("click", ".addButton", function(){ 
+        var imdbID = $(this).data('id');
+        var movie = movieData.find(function(currentMovie) {
+            return currentMovie.imdbID == imdbID;
+        });
+        var watchlistJSON = localStorage.getItem('watchlist');
+        var watchlist = JSON.parse(watchlistJSON);
+        if (watchlist === null) {
+            watchlist = [];
+            console.log('list action');
+        };
+        watchlist.push(movie);
+        watchlistJSON = JSON.stringify(watchlist);
+        localStorage.setItem('watchlist', watchlistJSON);
     });
 });
---
+++ watchlist.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+    localStorage.getItem('watchlist');
+    var watchlistJSON = localStorage.getItem('watchlist');
+    var watchlist = JSON.parse(watchlistJSON);
+    function renderMovies(movieArray) {
+        var finalHTML = '';
+
+        movieArray.forEach(function(currentMovie) {
+            finalHTML += '<div class="movie card">';
+            finalHTML += '<img src="' + currentMovie.Poster + '" class="card-img-top" />';
+            finalHTML += '<div class="card-body">';
+            finalHTML += '<h5 class="card-title movieTitle">' + currentMovie.Title + '</h5>';
+            finalHTML += '<h6 class="card-subtitle text-muted movieYear">' + currentMovie.Year + '</h6>';
+            finalHTML += '<button type="button" class="btn-primary addButton" data-id="' +currentMovie.imdbID + '">Add!</button>';
+            finalHTML += '</div>';
+            finalHTML += '</div>';
+            
+        }); 
+        return finalHTML;
+    }
+    var movieHTML = renderMovies(watchlist);
+    $('.movies-container').html(movieHTML);
+});
